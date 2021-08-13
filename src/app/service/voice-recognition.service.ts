04b80ba4532355fffde121155e68d675d118ea21
Robert L <robertmorales181@gmail.com>
2021-08-12 18:37:45 -0600

    Correccion espacios en blanco y texto inmediato en la vista

--- src/app/service/voice-recognition.service.ts
+++ src/app/service/voice-recognition.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 
-
+//webkit api Google
 declare var webkitSpeechRecognition: any;
 
 @Injectable({
@@ -8,7 +8,8 @@ declare var webkitSpeechRecognition: any;
 })
 export class VoiceRecognitionService {
 
- recognition =  new webkitSpeechRecognition();
+
+  recognition = new webkitSpeechRecognition();
   isStoppedSpeechRecog = false;
   public text = '';
   public lastText = 'Reconocimiento de voz.';
@@ -28,7 +29,31 @@ export class VoiceRecognitionService {
         .join('');
       this.tempWords = transcript;
       //this.text = transcript;
-      this.lastText = transcript;
+
+      //Numero de palabras escuchadas
+      var numWords = this.countString(transcript);
+      //numero de plabaras visbles
+      var numWordsClient = 10;
+
+      //si hay más de 10 palabaras, mostrar las ultimas 10
+      if (numWords > numWordsClient) {
+        var __text = this.correctText(transcript);
+        var _text_ = __text.split(' ', numWords);
+        var aNew = _text_.slice(numWords - numWordsClient);
+        var newText = '';
+
+        for (var i = 0; i < aNew.length; i++) {
+          newText = newText + aNew[i] + " ";
+        }
+
+        // console.log(numWords + " " + newText);
+        this.lastText = newText;
+      } else {
+        // console.log(numWords + " " + transcript);
+        this.lastText = transcript;
+      }
+
+      //this.lastText = transcript;
       console.log(transcript);
     });
   }
@@ -58,11 +83,40 @@ export class VoiceRecognitionService {
   wordConcat() {
     this.text = this.text + this.tempWords + ' ';
     this.tempWords = '';
+    this.text = this.correctText(this.text);
   }
 
-  clearRecord(){
+  clearRecord() {
     this.text = '';
   }
+
+
+  //Correccion de espacios en blanco
+  correctText(text: string) {
+    text = text.replace(/[ ]+/g, " ");
+    text = text.replace(/^ /, "");
+    text = text.replace(/ $/, "");
+
+    return text;
+  }
+
+  lineEnter() {
+    this.stop();
+    this.text += '\n';
+  }
+
+  //devuelve numero de palabras de un string
+  countString(text: string) {
+    text = text.replace(/\r?\n/g, " ");
+    text = text.replace(/[ ]+/g, " ");
+    text = text.replace(/^ /, "");
+    text = text.replace(/ $/, "");
+    var _text = text.split(" ");
+    var numWords = _text.length;
+
+    return numWords;
+  }
+
 }
 
 
