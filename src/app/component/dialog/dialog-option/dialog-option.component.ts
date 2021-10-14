6a12754cf88a144f4335a1e4a4a9f1fa00faec31
Robert L <robertmorales181@gmail.com>
2021-10-14 11:41:50 -0600

    Select funcional

--- src/app/component/dialog/dialog-option/dialog-option.component.ts
+++ src/app/component/dialog/dialog-option/dialog-option.component.ts
@@ -1,13 +1,27 @@
 import { Component, OnInit,Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import	 {DialogData} from 'src/app/interfaces/dialog.interface'
+import	 {DialogData} from 'src/app/interfaces/dialog.interface';
+
+interface Food {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-dialog-option',
   templateUrl: './dialog-option.component.html',
   styleUrls: ['./dialog-option.component.scss']
 })
-export class DialogOptionComponent implements OnInit {
 
+export class DialogOptionComponent implements OnInit {
+  selectedValue: string = "steak-0";
+
+  foods: Food[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'}
+  ];
+  copies = 1;
   favoriteSeason: string;
   seasons: string[] = [];
 
